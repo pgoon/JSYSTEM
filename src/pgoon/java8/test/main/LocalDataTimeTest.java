e5a9a6abfcae86d0283cef65c7590089b55cd45d
pgoon <pgoon@hotmail.com>
2016-03-22 10:30:32 +0900

    Update LocalDataTimeTest.java

--- src/pgoon/java8/test/main/LocalDataTimeTest.java
+++ src/pgoon/java8/test/main/LocalDataTimeTest.java
@@ -21,9 +21,9 @@ public class LocalDataTimeTest {
 		System.out.println(ldt);
 		System.out.println(ldt2);
 
-		System.out.println(ChronoUnit.YEARS.between(ldt, ldt2) + "”N");
-		System.out.println(ChronoUnit.MONTHS.between(ldt, ldt2) + "ŒŽ");
-		System.out.println(ChronoUnit.DAYS.between(ldt, ldt2) + "“ú");
+		System.out.println(ChronoUnit.YEARS.between(ldt, ldt2) + "å¹´");
+		System.out.println(ChronoUnit.MONTHS.between(ldt, ldt2) + "æœˆ");
+		System.out.println(ChronoUnit.DAYS.between(ldt, ldt2) + "æ—¥");
 
 		ldt3 = ldt2.plusMonths(2)
 			   .minusDays(10);
@@ -31,7 +31,7 @@ public class LocalDataTimeTest {
 		System.out.println(ldt3);
 		System.out.println(JapaneseDate.now());
 		System.out.println(
-				DateTimeFormatter.ofPattern("Gy”NMŒŽd“ú")
+				DateTimeFormatter.ofPattern("Gyå¹´Mæœˆdæ—¥")
 				.withChronology(JapaneseChronology.INSTANCE)
 				.format(JapaneseDate.of(2016, 3, 21))
 				);
